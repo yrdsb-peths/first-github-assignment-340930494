246e61cba6fa9c80daa57667b0947fb105896f57
Jacky Liang <117941640+340930494@users.noreply.github.com>
2022-11-14 13:57:57 -0500

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,28 @@
+private int l;
+private int w;
+
+public Rectangle(int length, int width)
+{
+  l = length;
+  w = width;
+}
+
+private void getLength()
+{
+  return l;
+}
+
+private void getWidth()
+{
+  return w;
+}
+
+private void getArea()
+{
+  return l * w;
+}
+
+private void toString()
+{
+  return "";
+}
